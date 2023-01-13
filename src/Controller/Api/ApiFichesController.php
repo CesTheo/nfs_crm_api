dce4f065c0895a8fb4bafde43480a68b9fb5a181
cestheo <theomaudet03@gmail.com>
2023-01-13 11:22:06 +0100

    push devis facture part1

--- src/Controller/Api/ApiFichesController.php
+++ src/Controller/Api/ApiFichesController.php
@@ -8,6 +8,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Helper\HttpResponseHelper;
+use Dompdf\Dompdf;
+use Symfony\Component\HttpFoundation\Response;
 
 #[Route('/fiches', name: 'fiches')]
 class ApiFichesController extends AbstractController
@@ -21,9 +23,23 @@ class ApiFichesController extends AbstractController
     }
 
     #[Route('/test', name: '_test')]
-    function getDataUser(): JsonResponse
+    public function getDataUser(): Response
     {
-        $response = $this->ficheService->createDevisExample();
-        return $this->json(HttpResponseHelper::success($response));
+        // $response = $this->ficheService->createDevisExample();
+        // return $this->json(HttpResponseHelper::success($response));
+        $dompdf = new Dompdf();
+        $html = '<h1>Hello World</h1>'; // contenu HTML
+        $dompdf->loadHtml($html);
+        $dompdf->render();
+        $pdf = $dompdf->output();
+        return new Response($pdf, 200, array(
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'attachment; filename="file.pdf"'
+        ));
     }
+    //Création fiche
+    //Lire la fiche
+    //Supprimer fiche
+    //Modifier une fiche (facultatif)
+    //telecharger la fiche via pdf
 }
--- src/Services/FicheService.php
+++ src/Services/FicheService.php
@@ -16,11 +16,57 @@ class FicheService
     public function createDevisExample(){
         $datapdf = [
             [
-                "information"=> [
-                    "tesaat" => "dddd",
-                    "tessssst" => "dddsdqsdqd",
-                    "tessst" => "dddsdqdd",
-                    "test" => "dqsdqsdddd",
+                "lead"=> [
+                    "nom" => "Talode",
+                    "numero_identification_tva" => "0000001",
+                    "numero_de_bon_de_commandes" => "0012023",
+                    "status" => "aaa",
+                    "raison_sociale" => "SAS",
+                    "forme_juridique" => "dqsdqsdddd",
+                    "adresse_entreprise" => "dqsdqsdddd",
+                    "rcs_ou_numero_repoertoire_des_metiers" => "dqsdqsdddd",
+                    "date_du_projet" => "dqsdqsdddd",
+                    "siren" => "dqsdqsdddd",
+                ],
+                "finance" => [
+                    "prix_horaire" => "",
+                    "forfaitaire_main_doeuvre" => "",
+                    "frais_de_deplacement" => "",
+                    "conditions_paiment" => "",
+                    "livraison_execution_du_contrat" => "",
+                    "procedure_reclamation" => "",
+                    "service_apres_vente" => "",
+                    "total_ht" => "",
+                    "total_tcc" => "",
+                    "remise" => "",
+                    "tva_applicables" => "",
+                ],
+                "customer" => [
+                    "adresse_facturation" => "",
+                    "nom_du_client" => "",
+                    "adresse_client" => "",
+                    "date_debut_contrat" => "",
+                    "date_fin_contrat" => "",
+                ],
+                "details" => [
+                    1 => [
+                        "nom_prestation" => "",
+                        "quantite" => "",
+                        "prix_unitaire_ht" => "",
+                    ],
+                    2 => [
+                        "nom_prestation" => "",
+                        "quantite" => "",
+                        "prix_unitaire_ht" => "",
+                    ],
+                ],
+                "facture" => [
+                    "date_facturation" => "",
+                    "penalite_retard" => "",
+                    "indemnite_forfaitaire" => "",
+                ],
+                "devis" => [
+                    "duree_validite_devis" => ""
                 ]
             ]
         ];
